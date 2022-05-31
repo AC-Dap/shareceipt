62b862b25bed1a9562df1af3476afca1b986321f
Anthony Cui Desktop <ninjaminer18@gmail.com>
2022-05-30 20:32:17 -0400

    Change dialog for tip and tax to accept percentage or actual dollar amount

--- components/ReceiptOverviewFooter.tsx
+++ components/ReceiptOverviewFooter.tsx
@@ -3,6 +3,7 @@ import {ReceiptItemType} from "../screens/ReceiptSplitScreen";
 import {useMemo, useState} from "react";
 import TextInputDialog from "./TextInputDialog";
 import {Text, Banner} from "./theming";
+import {Button, Dialog, Portal, TextInput} from "react-native-paper";
 
 type ReceiptOverviewFooterProps = {
     items: ReceiptItemType[],
@@ -10,6 +11,63 @@ type ReceiptOverviewFooterProps = {
     tipPercent: number,
     setTaxPercent: (newVal: number) => void,
     setTipPercent: (newVal: number) => void
+}
+
+type PercentAmountDialogProps = {
+    visible: boolean,
+    title: string,
+    total: number,
+    onClose: () => void,
+    onSubmit: (newVal: number) => void
+}
+
+function PercentAmountDialog({ visible, title, total, onClose, onSubmit } : PercentAmountDialogProps){
+    const [percent, setPercent] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const close = () => {
+        onClose();
+        setPercent("");
+        setAmount("");
+    }
+    const submit = () => {
+        onSubmit((percent == "")? 0 : parseFloat(percent));
+        close();
+    }
+    const newPercent = (val: string) => {
+        setPercent(val);
+        if(val == "") setAmount("");
+        else setAmount((parseFloat(val) / 100 * total).toFixed(2));
+    }
+    const newAmount = (val: string) => {
+        setAmount(val);
+        if(val == "") setPercent("");
+        else setPercent((parseFloat(val) / total * 100).toFixed(2));
+    }
+
+    return (
+        <Dialog visible={visible} onDismiss={onClose}>
+            <Dialog.Title><Text>{title}</Text></Dialog.Title>
+            <Dialog.Content>
+                <TextInput
+                    label={"Percent"}
+                    value={percent}
+                    onChangeText={inp => newPercent(inp)}
+                    keyboardType={"numeric"}
+                />
+                <TextInput
+                    label={"Dollar Amount"}
+                    value={amount}
+                    onChangeText={inp => newAmount(inp)}
+                    keyboardType={"numeric"}
+                />
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={close}><Text>Cancel</Text></Button>
+                <Button onPress={submit}><Text>Submit</Text></Button>
+            </Dialog.Actions>
+        </Dialog>
+    );
 }
 
 export default function ReceiptOverviewFooter({ items, taxPercent, tipPercent, setTaxPercent, setTipPercent } : ReceiptOverviewFooterProps){
@@ -34,21 +92,23 @@ export default function ReceiptOverviewFooter({ items, taxPercent, tipPercent, s
             </TouchableOpacity>
             <Text>{`Total: $${(itemTotal * (1 + (taxPercent + tipPercent)/ 100)).toFixed(2)}`}</Text>
 
-            <TextInputDialog
-                title={"Edit Tax Percentage"} subtitle={"Tax"}
-                visible={showTaxDialog}
-                onClose={() => setShowTaxDialog(false)}
-                submitInput={(inp) => setTaxPercent(parseFloat(inp))}
-                keyboardType={"numeric"}
-            />
+            <Portal>
+                <PercentAmountDialog
+                    visible={showTaxDialog}
+                    title={"Edit Tax"}
+                    total={itemTotal}
+                    onClose={() => setShowTaxDialog(false)}
+                    onSubmit={(newVal) => setTaxPercent(newVal)}
+                />
 
-            <TextInputDialog
-                title={"Edit Tip Percentage"} subtitle={"Tip"}
-                visible={showTipDialog}
-                onClose={() => setShowTipDialog(false)}
-                submitInput={(inp) => setTipPercent(parseFloat(inp))}
-                keyboardType={"numeric"}
-            />
+                <PercentAmountDialog
+                    visible={showTipDialog}
+                    title={"Edit Tip"}
+                    total={itemTotal}
+                    onClose={() => setShowTipDialog(false)}
+                    onSubmit={(newVal) => setTipPercent(newVal)}
+                />
+            </Portal>
         </Banner>
     );
 }
